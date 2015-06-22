dc58113ad4b77818b1d86c2edeabdb306c1d0ad9
Brandon <brandon@localhost.localdomain>
2015-06-22 16:14:29 -0400

    firebase connect

--- app/src/main/java/edu/msu/bielick3/cataapp/MainActivity.java
+++ app/src/main/java/edu/msu/bielick3/cataapp/MainActivity.java
@@ -4,11 +4,17 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.webkit.WebView;
 
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.Query;
+import com.firebase.client.ValueEventListener;
 import com.google.android.gms.maps.MapFragment;
 import com.mapbox.mapboxsdk.api.ILatLng;
 import com.mapbox.mapboxsdk.geometry.LatLng;
@@ -20,6 +26,11 @@ import com.mapbox.mapboxsdk.views.MapView;
 import com.mapbox.mapboxsdk.views.MapViewListener;
 import com.mapbox.mapboxsdk.views.util.TilesLoadedListener;
 
+import java.lang.reflect.Array;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
 
 public class MainActivity extends Activity {
 
@@ -30,6 +41,7 @@ public class MainActivity extends Activity {
     private MapView mv;
     private UserLocationOverlay myLocationOverlay;
     private String currentMap = null;
+    private int route;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +50,61 @@ public class MainActivity extends Activity {
 
         mv = (MapView) findViewById(R.id.mapview);
         mv.setUserLocationEnabled(true);
-        mv.setUserLocationTrackingMode(UserLocationOverlay.TrackingMode.FOLLOW_BEARING);
+        //mv.setUserLocationTrackingMode(UserLocationOverlay.TrackingMode.FOLLOW_BEARING);
+        mv.setCenter(new ILatLng() {
+            @Override
+            public double getLatitude() {
+                return 42.7369792;
+            }
+
+            @Override
+            public double getLongitude() {
+                return -84.48386540000001;
+            }
+
+            @Override
+            public double getAltitude() {
+                return 0;
+            }
+        });
+        mv.setZoom(13);
+
+        Firebase.setAndroidContext(this);
+        Firebase ref = new Firebase("https://sizzling-fire-5776.firebaseio.com/routes");
+
+        Query query = ref.orderByChild("RouteNumber").equalTo("26");
+
+        query.addValueEventListener(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(dataSnapshot.getValue() != null) {
+                    Log.d("TEST", dataSnapshot.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
     }
 
+    public void routeSelect(View view) {
+        Intent intent = new Intent(this, routeSelect.class);
+        startActivityForResult(intent, 200);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Check which request we're responding to
+        if (requestCode == 200) {
+            if (resultCode == RESULT_OK) {
+                route = data.getIntExtra("route", 1);
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
