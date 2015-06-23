cd4a893bb45ff3617615f71b535f6aadb41eb663
BrandonBielicki <brandonbielicki@gmail.com>
2015-06-23 08:51:03 -0700

    Working on gridview for routes

--- app/src/main/java/edu/msu/bielick3/cataapp/routeSelect.java
+++ app/src/main/java/edu/msu/bielick3/cataapp/routeSelect.java
@@ -1,10 +1,21 @@
 package edu.msu.bielick3.cataapp;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.NumberPicker;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class routeSelect extends Activity {
@@ -13,6 +24,49 @@ public class routeSelect extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_select);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new adapter());
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+
+            }
+        });
+
+
+    }
+
+    public class adapter extends BaseAdapter {
+        private Context mContext;
+
+        public int getCount() {
+            return stringArray.length;
+        }
+
+        public Object getItem(int position) {
+            return null;
+        }
+
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        public View getView(int position, View convertView, ViewGroup parent) {
+            
+        }
+
+        private String[] stringArray = getResources().getStringArray(R.array.routes_array);
+    }
+
+    public void returnRoute(View view) {
+
+    }
+
+    public void cancelSelect(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
